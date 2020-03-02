db62729b90fa36ab58e06c5251c4b4f956fab3b0
fritzgt <tuanis@live.com>
2020-03-02 18:14:51 +0100

    remove yarn

---
+++ build/precache-manifest.36e91ccb2910934807c0f13ab85fbf23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8d35ab56216cc1d36efcf80ed5b5b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9382c5d1336aca9040b0",
+    "url": "/static/css/main.3f4ac37a.chunk.css"
+  },
+  {
+    "revision": "b0a72a59a1c3d154d0b2",
+    "url": "/static/js/2.34bfcf11.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.34bfcf11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9382c5d1336aca9040b0",
+    "url": "/static/js/main.8e1bdabe.chunk.js"
+  },
+  {
+    "revision": "ea94fe1a8c57b6d0c89f",
+    "url": "/static/js/runtime-main.13ea8918.js"
+  },
+  {
+    "revision": "fc297084b3c694a2039a912ca1a66462",
+    "url": "/static/media/logo2.fc297084.png"
+  }
+]);
